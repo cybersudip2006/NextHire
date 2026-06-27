256a0e5110875dfd602c4b302332ff5c0ec9b4d6
SUDIP CHAKRABORTY <sudipchakraborty81010@gmail.com>
2026-06-27 13:57:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,31 +1,23 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
-# Create SQLAlchemy object
 db = SQLAlchemy()
 
 
 class ResumeData(db.Model):
     __tablename__ = "resume_data"
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
+    id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(
-        db.String(100),
-        nullable=False
-    )
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(25), nullable=False)
 
-    email = db.Column(
-        db.String(120),
-        nullable=False
-    )
+    template_style = db.Column(db.String(50), default="ats")
+    filename = db.Column(db.String(255), nullable=True)
+    ats_score = db.Column(db.Float, nullable=True)
 
-    phone = db.Column(
-        db.String(25),
-        nullable=False
-    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<ResumeData {self.name}>"
